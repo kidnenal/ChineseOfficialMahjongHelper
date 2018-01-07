081f7e8e3a5b6e65fe3a6e34367618516d7957e1
Jeff Wang <summer_insects@163.com>
2018-01-07 17:37:28 +0800

    no message

--- Classes/UICommon.h
+++ Classes/UICommon.h
@@ -7,15 +7,15 @@
 namespace UICommon {
 
     static inline cocos2d::ui::Button *createButton() {
-        return cocos2d::ui::Button::create("source_material/btn_square_highlighted.png", "source_material/btn_square_selected.png");
+        return cocos2d::ui::Button::create("source_material/btn_square_highlighted.png", "source_material/btn_square_selected.png", "source_material/btn_square_disabled.png");
     }
 
     static inline cocos2d::ui::CheckBox *createCheckBox() {
-        return cocos2d::ui::CheckBox::create("source_material/btn_square_normal.png", "source_material/btn_square_highlighted.png");
+        return cocos2d::ui::CheckBox::create("source_material/btn_square_normal.png", "", "source_material/btn_square_highlighted.png", "source_material/btn_square_disabled.png", "source_material/btn_square_disabled.png");
     }
 
     static inline cocos2d::ui::RadioButton *createRadioButton() {
-        return cocos2d::ui::RadioButton::create("source_material/btn_square_normal.png", "source_material/btn_square_highlighted.png");
+        return cocos2d::ui::RadioButton::create("source_material/btn_square_normal.png", "", "source_material/btn_square_highlighted.png", "source_material/btn_square_disabled.png", "source_material/btn_square_disabled.png");
     }
 }
 
